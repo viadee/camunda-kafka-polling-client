a071c044f23e82eb2f87fd7caaccaf472902cbc0
Daniel Alile <daniel.alile@smail.th-koeln.de>
2021-03-20 16:25:38 +0100

    added annotations

--- camunda-kafka-model/src/main/java/de/viadee/camunda/kafka/event/DecisionInstanceEvent.java
+++ camunda-kafka-model/src/main/java/de/viadee/camunda/kafka/event/DecisionInstanceEvent.java
@@ -14,66 +14,82 @@ public class DecisionInstanceEvent extends DetailEvent {
     private List<DecisionInstanceInputEvent> inputs;
     private List<DecisionInstanceOutputEvent> outputs;
 
+    @java.lang.SuppressWarnings("all")
     public List<DecisionInstanceInputEvent> getInputs() {
         return inputs;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setInputs(List<DecisionInstanceInputEvent> inputs) {
         this.inputs = inputs;
     }
 
+    @java.lang.SuppressWarnings("all")
     public List<DecisionInstanceOutputEvent> getOutputs() {
         return outputs;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setOutputs(List<DecisionInstanceOutputEvent> outputs) {
         this.outputs = outputs;
     }
 
+    @java.lang.SuppressWarnings("all")
     public String getDecisionDefinitionId() {
         return decisionDefinitionId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setDecisionDefinitionId(String decisionDefinitionId) {
         this.decisionDefinitionId = decisionDefinitionId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public String getDecisionDefinitionKey() {
         return decisionDefinitionKey;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setDecisionDefinitionKey(String decisionDefinitionKey) {
         this.decisionDefinitionKey = decisionDefinitionKey;
     }
 
+    @java.lang.SuppressWarnings("all")
     public String getDecisionDefinitionName() {
         return decisionDefinitionName;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setDecisionDefinitionName(String decisionDefinitionName) {
         this.decisionDefinitionName = decisionDefinitionName;
     }
 
+    @java.lang.SuppressWarnings("all")
     public String getActivityId() {
         return activityId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setActivityId(String activityId) {
         this.activityId = activityId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public String getActivityInstanceId() {
         return activityInstanceId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setActivityInstanceId(String activityInstanceId) {
         this.activityInstanceId = activityInstanceId;
     }
 
+    @java.lang.SuppressWarnings("all")
     public Date getEvaluationTime() {
         return evaluationTime;
     }
 
+    @java.lang.SuppressWarnings("all")
     public void setEvaluationTime(Date evaluationTime) {
         this.evaluationTime = evaluationTime;
     }
