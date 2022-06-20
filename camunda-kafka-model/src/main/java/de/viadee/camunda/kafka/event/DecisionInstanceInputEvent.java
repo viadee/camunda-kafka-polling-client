f4d3a7241718d6d78fe409c68ab2a000b7fea5ee
Daniel Alile <12607126+tDalile@users.noreply.github.com>
2022-06-20 12:29:53 +0200

    Add layout classes for decision instance event mapping
    Add decision instance class
    
    Add decision instance input and output class
    
    Add decision instance response class
    
    Add decision instance input and output response class
    
    Fix unnecessary blank line

---
+++ camunda-kafka-model/src/main/java/de/viadee/camunda/kafka/event/DecisionInstanceInputEvent.java
@@ -0,0 +1,106 @@
+package de.viadee.camunda.kafka.event;
+
+import java.util.Map;
+
+public class DecisionInstanceInputEvent {
+
+        private String id;
+        private String decisionInstanceId;
+        private String clauseId;
+        private String clauseName;
+        private String errorMessage;
+        private String type;
+        private String createTime;
+        private String removalTime;
+        private String rootProcessInstanceId;
+        private String value;
+        private Map<String, Object> valueInfo;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getDecisionInstanceId() {
+            return decisionInstanceId;
+        }
+
+        public void setDecisionInstanceId(String decisionInstanceId) {
+            this.decisionInstanceId = decisionInstanceId;
+        }
+
+        public String getClauseId() {
+            return clauseId;
+        }
+
+        public void setClauseId(String clauseId) {
+            this.clauseId = clauseId;
+        }
+
+        public String getClauseName() {
+            return clauseName;
+        }
+
+        public void setClauseName(String clauseName) {
+            this.clauseName = clauseName;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getCreateTime() {
+            return createTime;
+        }
+
+        public void setCreateTime(String createTime) {
+            this.createTime = createTime;
+        }
+
+        public String getRemovalTime() {
+            return removalTime;
+        }
+
+        public void setRemovalTime(String removalTime) {
+            this.removalTime = removalTime;
+        }
+
+        public String getRootProcessInstanceId() {
+            return rootProcessInstanceId;
+        }
+
+        public void setRootProcessInstanceId(String rootProcessInstanceId) {
+            this.rootProcessInstanceId = rootProcessInstanceId;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public Map<String, Object> getValueInfo() {
+        return valueInfo;
+    }
+
+        public void setValueInfo(Map<String, Object> valueInfo) {
+        this.valueInfo = valueInfo;
+    }
+}
